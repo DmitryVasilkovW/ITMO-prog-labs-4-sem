a478deebe0773a6abb4a0b8f621bfd3001cb1f85
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-02-11 17:56:40 +0300

    fix: output

--- src/Tasks/Practitioners/Practice1/Task2/Main.java
+++ src/Tasks/Practitioners/Practice1/Task2/Main.java
@@ -13,14 +13,14 @@ public  class Main
         double dou = 0.2333339;
         long lo = 233333339L;
 
-        System.out.println(num + "\n");
-        System.out.println(str + "\n");
-        System.out.println(bool + "\n");
-        System.out.println(sh + "\n");
-        System.out.println(ch + "\n");
-        System.out.println(fl + "\n");
-        System.out.println(dou + "\n");
-        System.out.println(lo + "\n");
+        System.out.println(num);
+        System.out.println(str);
+        System.out.println(bool);
+        System.out.println(sh);
+        System.out.println(ch);
+        System.out.println(fl);
+        System.out.println(dou);
+        System.out.println(lo);
 
 
         byte v_byte = 120;
@@ -32,13 +32,15 @@ public  class Main
         double v_double = 0.3333333333333333;
         boolean v_double239 = true;
 
-        System.out.println(v_byte + 1 + "\n");
-        System.out.println(v_short + 1 + "\n");
-        System.out.println(v_char + "\n");
-        System.out.println(v_int + 1 + "\n");
-        System.out.println(v_long + 1 + "\n");
-        System.out.println(v_float + 0.239 + "\n");
-        System.out.println(v_double + 0.239 + "\n");
-        System.out.println(v_double239 + "\n");
+        System.out.println();
+
+        System.out.println(v_byte + 1);
+        System.out.println(v_short + 1);
+        System.out.println(v_char);
+        System.out.println(v_int + 1);
+        System.out.println(v_long + 1);
+        System.out.println(v_float + 0.239);
+        System.out.println(v_double + 0.239);
+        System.out.println(v_double239);
     }
 }
