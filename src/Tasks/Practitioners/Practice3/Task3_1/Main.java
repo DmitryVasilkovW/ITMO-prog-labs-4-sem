9630f490533d1f49d1948a8b111e7876663c34ba
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-02-11 21:54:13 +0300

    feat: Second task

---
+++ src/Tasks/Practitioners/Practice3/Task3_1/Main.java
@@ -0,0 +1,29 @@
+package Tasks.Practitioners.Practice3.Task3_1;
+
+import Tasks.Practitioners.Practice2.Task1_2.Category;
+import Tasks.Practitioners.Practice2.Task2_2.FoodItem;
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        String line = "Конфеты ’Маска’;45;120";
+
+
+        String[] item_fld;
+
+
+        item_fld = line.split(";");
+
+
+        int productid = 1;
+        Category category = Category.Food;
+        Date dateOfIncome = new Date();
+        short expires = 10;
+
+        var foodItem = new FoodItem(productid, item_fld[0], Double.parseDouble(item_fld[1]), category, dateOfIncome, expires);
+        foodItem.PrintAll();
+    }
+}
