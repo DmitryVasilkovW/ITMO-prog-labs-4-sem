ed73c6cdbf1ad4ae87f5a14a28f968e603f420e0
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-02-13 19:10:44 +0300

    feat: second lesson

---
+++ src/Tasks/Practitioners/Practice5/Lesson2/BigDecimalChecker.groovy
@@ -0,0 +1,8 @@
+package Tasks.Practitioners.Practice5.Lesson2
+
+BigDecimal num1 = new BigDecimal("123.45")
+BigDecimal num2 = new BigDecimal("123.45")
+
+println(num1 == num2)
+println(num1.equals(num2))
+println(num1.is(num2))
