a60f60f2fb1ca8d63c6cadbae141f04aa012045f
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-03-23 18:51:00 +0300

    refactor: database access

--- Labs/src/Lab1/src/main/java/Database/Initial.java
+++ Labs/src/Lab1/src/main/java/Database/Initial.java
@@ -1,18 +1,21 @@
 package Database;
 
+import Database.Connection.DatabaseLocalHostSingleton;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseTableCreator
+public class Initial
 {
     private DatabaseLocalHostSingleton _databaseSingleton;
 
-    public DatabaseTableCreator()
+    public Initial()
     {
         try
         {
-            this._databaseSingleton = DatabaseLocalHostSingleton.getInstance();
+            _databaseSingleton = DatabaseLocalHostSingleton.getInstance();
         }
         catch (SQLException e)
         {
@@ -20,6 +23,7 @@ public class DatabaseTableCreator
         }
     }
 
+    @Transactional
     public void GetUpSql()
     {
         String createTablesSQL =
@@ -46,13 +50,46 @@ public class DatabaseTableCreator
                 + "UserId INT,"
                 + "FOREIGN KEY (UserId) REFERENCES users(Id),"
                 + "Series INT NOT NULL ,"
-                + "Number INT NOT NULL);";
+                + "Number INT NOT NULL);"
+
+                + "CREATE TABLE "
+                + "accounts" + "("
+                + "id SERIAL PRIMARY KEY NOT NULL,"
+                + "UserId INT,"
+                + "Type TEXT NOT NULL,"
+                + "creditLimit NUMERIC, "
+                + "commission NUMERIC,"
+                + "interestRate NUMERIC,"
+                + "depositEndDate DATE,"
+                + "FOREIGN KEY (UserId) REFERENCES users(Id),"
+                + "balance NUMERIC NOT NULL DEFAULT 0);";
 
         try
         {
             Connection connection = this._databaseSingleton.getConnection();
             Statement stmt = connection.createStatement();
             stmt.executeUpdate(createTablesSQL);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Transactional
+    public void GetDownSql()
+    {
+        String dropTablesSQL =
+                "drop table if exists accounts;" +
+                "drop table if exists address;" +
+                "drop table if exists PassportDetails;" +
+                "drop table if exists users";
+
+        try
+        {
+            Connection connection = this._databaseSingleton.getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(dropTablesSQL);
         }
         catch (SQLException e)
         {
