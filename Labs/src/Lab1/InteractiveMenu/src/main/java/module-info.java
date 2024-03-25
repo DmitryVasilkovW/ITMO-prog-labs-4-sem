7bc26291b826003db8f022b859548ecd5e9b9744
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-03-25 21:11:24 +0300

    влад помоги

--- Labs/src/Lab1/InteractiveMenu/src/main/java/module-info.java
+++ Labs/src/Lab1/InteractiveMenu/src/main/java/module-info.java
@@ -12,6 +12,11 @@ module com.menu.interactivemenu
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
+    requires Lab1;
+
     opens com.menu.interactivemenu to javafx.fxml;
     exports com.menu.interactivemenu;
+    opens Scenarios to javafx.fxml;
+    exports Scenarios;
 }
+
